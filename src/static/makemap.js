cef5d0b35ddd01326ba480c4d30d83f35d478884
ncaldwell17 <ncaldwellgatsos@gmail.com>
2020-04-14 03:37:27 -0500

    new dashboard

--- src/static/makemap.js
+++ src/static/makemap.js
@@ -1,3 +1,63 @@
+function renderChloropleth() {
+
+    let mapDiv = document.getElementById("graphicContainer");
+    let svg = d3.select(mapDiv).append("svg");
+
+    $.getJSON('../static/resources/scores/ordered_hpb.json', function (data) {
+
+        // alter SVG
+        svg
+            .attr("width", 700)
+            .attr("height", 600);
+
+        // Append empty placeholder g element to the SVG
+        // g will contain geometry elements
+        const g = svg.append("g");
+
+        // width and height of the whole visualization by
+        // setting projection parameters
+        let albersProjection = d3.geoAlbers()
+            .scale(82000)
+            .rotate([87.5900, 0])
+            .center([0, 41.8781])
+        // .translate( [width, height] );
+
+        // create GeoPath function that uses built-in D3 functionality to turn
+        // lat/lon coordinates into screen coordinates (i.e. path generator)
+        let geoPath = d3.geoPath()
+            .projection(albersProjection);
+
+        var color = d3.scaleThreshold()
+            .domain([0, 50, 100, 150, 200])
+            .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);
+
+        g.selectAll('path')
+            .data(data.features) // Bind data elements here
+            .enter().append("path")
+            .attr("d", geoPath)
+            .style("fill", function(d) {
+                return color(d.properties.count_homicides);
+            })
+            .style("stroke", "black");
+
+        g.selectAll('path')
+            .on("mouseover", function(d){
+                d3.select("h2").text("Beat No: "+d.properties.BEAT_ID);
+                d3.select(this).attr("class", "highlight");
+            })
+            .on("mouseout", function(d){
+                d3.select("h2").text("Beat No: ");
+                d3.select(this).attr("class", "blank");
+            })
+
+
+
+
+    })
+
+}
+
+
 function renderMap(input) {
 
     d3.selectAll("#container > *").remove();
@@ -26,7 +86,7 @@ function renderMap(input) {
         const albersProjection = d3.geoAlbers()
             .scale(82000)
             .rotate([87.6298, 0])
-            .center([0, 41.8781])
+            .center([0, 41.8781]);
         // .translate( [width, height] );
 
         // create GeoPath function that uses built-in D3 functionality to turn
