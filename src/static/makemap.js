2c7a707a28258e2514889ae962bf0272001e4fed
ncaldwell17 <ncaldwellgatsos@gmail.com>
2020-04-23 14:59:02 -0500

    mouseover functionality fixed

--- src/static/makemap.js
+++ src/static/makemap.js
@@ -1,3 +1,7 @@
+const color = d3.scaleThreshold()
+    .domain([0, 50, 100, 150, 200])
+    .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);
+
 function renderChloropleth() {
 
     let mapDiv = document.getElementById("graphicContainer");
@@ -27,30 +31,18 @@ function renderChloropleth() {
         let geoPath = d3.geoPath()
             .projection(albersProjection);
 
-        var color = d3.scaleThreshold()
-            .domain([0, 50, 100, 150, 200])
-            .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);
 
         g.selectAll('path')
             .data(data.features) // Bind data elements here
-            .enter().append("path")
+            .enter()
+            .append("path")
+            .attr("stroke", "#333")
             .attr("d", geoPath)
-            .style("fill", function(d) {
+            .attr("fill", function(d) {
                 return color(d.properties.count_homicides);
             })
-            .style("stroke", "black");
-
-        g.selectAll('path')
-            .on("mouseover", function(d){
-                d3.select("h2").text("Beat No: "+d.properties.BEAT_ID);
-                d3.select(this).attr("class", "highlight");
-            })
-            .on("mouseout", function(d){
-                d3.select("h2").text("Beat No: ");
-                d3.select(this).attr("class", "blank");
-            })
-
-
+            .on("mouseover", handleMouseOver)
+            .on("mouseout", handleMouseOut);
 
 
     })
@@ -58,104 +50,26 @@ function renderChloropleth() {
 }
 
 
-function renderMap(input) {
+function handleMouseOver(d, i) { //add interactivity here
 
-    d3.selectAll("#container > *").remove();
+    // use D3 to select the element, change its color and size
+    d3.select(this).attr("fill", "darkorange");
+    // specify where to put label or text or additional features
 
-    const mapDiv = document.getElementById("container");
-    const svg = d3.select(mapDiv).append("svg");
+}
 
-    $.getJSON(`../static/resources/${input}.json`, function (data) {
+function handleMouseOut(d, i) { //add interactivity here
 
-
-        // Width & Height of the Whole Visualization
-        // let width = mapDiv.clientWidth;
-        // let height = mapDiv.clientHeight;
-
-        // alter SVG
-        svg
-            .attr("width", 700)
-            .attr("height", 600);
-
-        // Append empty placeholder g element to the SVG
-        // g will contain geometry elements
-        const g = svg.append("g");
-
-        // width and height of the whole visualization by
-        // setting projection parameters
-        const albersProjection = d3.geoAlbers()
-            .scale(82000)
-            .rotate([87.6298, 0])
-            .center([0, 41.8781]);
-        // .translate( [width, height] );
-
-        // create GeoPath function that uses built-in D3 functionality to turn
-        // lat/lon coordinates into screen coordinates (i.e. path generator)
-        const geoPath = d3.geoPath()
-            .projection(albersProjection);
-
-        // Select non-existent elements, bind the data, append the elements, and apply attributes
-        g.selectAll("path")
-            .data(data.features)
-            .enter()
-            .append("path")
-            .attr("fill", "#ccc")
-            .attr("stroke", "#333")
-            .attr("d", geoPath);
-
-        if (input === 'zipcodes') {
-            g.selectAll("path")
-                .on("mouseover", function(d){
-                    d3.select("h2").text("Zip Code: "+d.properties.zip);
-                    d3.select(this).attr("class", "highlight");
-                })
-                .on("mouseout", function(d){
-                    d3.select("h2").text("Zip Code: ");
-                    d3.select(this).attr("class", "blank");
-                })
-        }
-
-        else if (input === 'districts') {
-            g.selectAll("path")
-                .on("mouseover", function(d){
-                    d3.select("h2").text("District: "+d.properties.DISTRICT);
-                    d3.select(this).attr("class", "highlight");
-                })
-                .on("mouseout", function(d){
-                    d3.select("h2").text("District: ");
-                    d3.select(this).attr("class", "blank");
-                })
-        }
-
-        else if (input === 'beats') {
-            g.selectAll("path")
-                .on("mouseover", function(d){
-                    d3.select("h2").text("Beat No: "+d.properties.BEAT_NO);
-                    d3.select(this).attr("class", "highlight");
-                })
-                .on("mouseout", function(d){
-                    d3.select("h2").text("Beat No: ");
-                    d3.select(this).attr("class", "blank");
-                })
-        }
-
-        else if (input === 'sectors') {
-            g.selectAll("path")
-                .on("mouseover", function(d){
-                    d3.select("h2").text("Sector: "+d.properties.SECTOR_NO);
-                    d3.select(this).attr("class", "highlight");
-                })
-                .on("mouseout", function(d){
-                    d3.select("h2").text("Sector: ");
-                    d3.select(this).attr("class", "blank");
-                })
-        }
-
-
+    // use D3 to select the element, change its color and size
+    d3.select(this).attr("fill", function(d) {
+        return color(d.properties.count_homicides);
     });
+    // specify what to do with the label or text or additional features
 
-    console.log('process completed, map should show up')
-    return "hello world";
-};
+
+}
+
+
+
 
 
---
+++ src/static/oldMakeMap.js
@@ -0,0 +1,99 @@
+function renderMap(input) {
+
+    d3.selectAll("#container > *").remove();
+
+    const mapDiv = document.getElementById("container");
+    const svg = d3.select(mapDiv).append("svg");
+
+    $.getJSON(`../static/resources/${input}.json`, function (data) {
+
+
+        // Width & Height of the Whole Visualization
+        // let width = mapDiv.clientWidth;
+        // let height = mapDiv.clientHeight;
+
+        // alter SVG
+        svg
+            .attr("width", 700)
+            .attr("height", 600);
+
+        // Append empty placeholder g element to the SVG
+        // g will contain geometry elements
+        const g = svg.append("g");
+
+        // width and height of the whole visualization by
+        // setting projection parameters
+        const albersProjection = d3.geoAlbers()
+            .scale(82000)
+            .rotate([87.6298, 0])
+            .center([0, 41.8781]);
+        // .translate( [width, height] );
+
+        // create GeoPath function that uses built-in D3 functionality to turn
+        // lat/lon coordinates into screen coordinates (i.e. path generator)
+        const geoPath = d3.geoPath()
+            .projection(albersProjection);
+
+        // Select non-existent elements, bind the data, append the elements, and apply attributes
+        g.selectAll("path")
+            .data(data.features)
+            .enter()
+            .append("path")
+            .attr("fill", "#ccc")
+            .attr("stroke", "#333")
+            .attr("d", geoPath);
+
+        if (input === 'zipcodes') {
+            g.selectAll("path")
+                .on("mouseover", function(d){
+                    d3.select("h2").text("Zip Code: "+d.properties.zip);
+                    d3.select(this).attr("class", "highlight");
+                })
+                .on("mouseout", function(d){
+                    d3.select("h2").text("Zip Code: ");
+                    d3.select(this).attr("class", "blank");
+                })
+        }
+
+        else if (input === 'districts') {
+            g.selectAll("path")
+                .on("mouseover", function(d){
+                    d3.select("h2").text("District: "+d.properties.DISTRICT);
+                    d3.select(this).attr("class", "highlight");
+                })
+                .on("mouseout", function(d){
+                    d3.select("h2").text("District: ");
+                    d3.select(this).attr("class", "blank");
+                })
+        }
+
+        else if (input === 'beats') {
+            g.selectAll("path")
+                .on("mouseover", function(d){
+                    d3.select("h2").text("Beat No: "+d.properties.BEAT_NO);
+                    d3.select(this).attr("class", "highlight");
+                })
+                .on("mouseout", function(d){
+                    d3.select("h2").text("Beat No: ");
+                    d3.select(this).attr("class", "blank");
+                })
+        }
+
+        else if (input === 'sectors') {
+            g.selectAll("path")
+                .on("mouseover", function(d){
+                    d3.select("h2").text("Sector: "+d.properties.SECTOR_NO);
+                    d3.select(this).attr("class", "highlight");
+                })
+                .on("mouseout", function(d){
+                    d3.select("h2").text("Sector: ");
+                    d3.select(this).attr("class", "blank");
+                })
+        }
+
+
+    });
+
+    console.log('process completed, map should show up')
+    return "hello world";
+};
